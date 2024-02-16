93714f02ab3cd3cba17ab398d66de2f57d5eb2c2
Fernando Rocha <fernandorochaworld@gmail.com>
2024-02-15 18:10:08 -0800

    Update: Adds currency not found check.

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express')  // We import the express application
-const cors = require('cors') // Necessary for localhost
-const app = express() // Creates an express application in app
+const express = require("express"); // We import the express application
+const cors = require("cors"); // Necessary for localhost
+const app = express(); // Creates an express application in app
 
 /**
  * Initial application setup
  * We need to use cors so we can connect to a localhost later
  * We need express.json so we can receive requests with JSON data attached
  */
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 /**
  * DATA STORAGE
  * We're using a local variable 'currencies' to store our data: a list of currency objects
  * Each object represents a 'currency', and contains the following fields
- * id: a number, 
+ * id: a number,
  * currencyCode: a string, three letters (see https://www.iban.com/currency-codes as reference)
  * country: a string, the name of the country
  * conversionRate: the amount, in that currency, required to equal 1 Canadian dollar
@@ -25,42 +24,47 @@ let currencies = [
     id: 1,
     currencyCode: "CDN",
     country: "Canada",
-    conversionRate: 1
+    conversionRate: 1,
   },
   {
     id: 2,
     currencyCode: "USD",
     country: "United States of America",
-    conversionRate: 0.75
-  }
-]
+    conversionRate: 0.75,
+  },
+];
 
 /**
  * TESTING Endpoint (Completed)
  * @receives a get request to the URL: http://localhost:3001/
  * @responds with the string 'Hello World!'
  */
-app.get('/', (request, response) => {
-  response.send('Hello World!')
-})
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
 
 /**
  * TODO: GET Endpoint
  * @receives a get request to the URL: http://localhost:3001/api/currency/
  * @responds with returning the data as a JSON
  */
-app.get('/api/currency/', (request, response) => {
-  response.json(currencies)
-})
+app.get("/api/currency/", (request, response) => {
+  response.json(currencies);
+});
 
 /**
  * TODO: GET:id Endpoint
  * @receives a get request to the URL: http://localhost:3001/api/currency/:id
  * @responds with returning specific data as a JSON
  */
-app.get('/api/currency/:id', (request, response) => {
-  response.status(200).json(currencies.find( item => item.id == request.params.id));
-})
+app.get("/api/currency/:id", (request, response) => {
+  const currency = currencies.find((item) => item.id == request.params.id);
+  if (currency) {
+    response.status(200).json(currency);
+  } else {
+    response.status(400).json({ message: "Currency not found." });
+  }
+});
 
 /**
  * TODO: POST Endpoint
@@ -68,10 +72,7 @@ app.get('/api/currency/:id', (request, response) => {
  * with data object enclosed
  * @responds by returning the newly created resource
  */
-app.post('...', (request, response) => {
-
-
-})
+app.post("...", (request, response) => {});
 
 /**
  * TODO: PUT:id endpoint
@@ -80,26 +81,20 @@ app.post('...', (request, response) => {
  * Hint: updates the currency with the new conversion rate
  * @responds by returning the newly updated resource
  */
-app.put('...', (request, response) => {
-  
-})
+app.put("...", (request, response) => {});
 
 /**
  * TODO: DELETE:id Endpoint
  * @receives a delete request to the URL: http://localhost:3001/api/currency/:id,
  * @responds by returning a status code of 204
  */
-app.post('...', (request, response) => {
+app.post("...", (request, response) => {});
 
+app.all("*", (req, res) => {
+  return res.status(404).json({ message: "unknown endpoint" }).end();
+});
 
-})
-
-        
-app.all('*', (req, res) => {
-  return res.status(404).json({message: 'unknown endpoint'}).end();
-})
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`)
-})
+  console.log(`Server running on port: ${PORT}`);
+});
