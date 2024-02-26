2acd90873e4a3166737c51128e7210d5c8fe1a57
Fernando Rocha <fernandorochaworld@gmail.com>
2024-02-26 02:41:30 -0800

    Test: Testing server pipeline.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ try {
 
   sequelize.sync().then(() => {
     api.listen(process.env.SERVER_PORT, () => {
-      console.log(`Server running on PORT: ${process.env.SERVER_PORT}`);
+      console.log(`Server running on port: ${process.env.SERVER_PORT}`);
     });
   });
 
